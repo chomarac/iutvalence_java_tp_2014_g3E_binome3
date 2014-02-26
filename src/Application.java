8591a445c3e7fb98129e203b0346aa1e9df71990
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-26 21:48:52 +0100

    TODO Javadoc (Application.java)

--- src/Application.java
+++ src/Application.java
@@ -1,10 +1,6 @@
 /* TODO Javadoc ! */
 public class Application {
-
 	public static void main(String[] args) {
-		//Permet de lancer le jeux
 		BN Bataille_Navale = new BN("tom","jerry");
-		
 	}
-
 }
