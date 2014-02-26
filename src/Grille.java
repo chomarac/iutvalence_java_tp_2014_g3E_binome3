f21ea35ce8c1d70d7c4f0bf576c879ac2f644393
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-26 21:49:53 +0100

    TODO Javadoc (Grille.java)

--- src/Grille.java
+++ src/Grille.java
@@ -1,35 +1,28 @@
-/* TODO Javadoc ! */
+/* TODO Javadoc ! Je vous mets un exemple. */
+
 /**
- * @author chomarac et andré
- *
+ * Grille de bataille navale.
  */
 public class Grille {
+    /* TODO Attention, en Java, par convention, on fait "static final". */
+    /* TODO Attention, en Java, par convention, on mets les noms de constantes en majuscule. */
+	/** Nombre de ligne d'une grille. */
+    private final static int Nombre_de_lignes   = 10;
+    /** Nombre de colonne d'une grille. */
+    private final static int Nombre_de_colonnes = 10;
+    /** Les cases de la grille. */
+    private Case[][] cases;
 
-	/**
-	 * Nombre de lignes de 1 à 10
-	 */
-	private final static int Nombre_de_lignes = 10;
-	
+    /**
+     * On fait une premiï¿½re grille
+     */
+    public Grille() {
+        this.cases = new Case[Nombre_de_lignes][Nombre_de_colonnes];
 
-	/**
-	 * Nombre de colonnes de A a J
-	 */
-	private final static int Nombre_de_colonnes = 10;
-	
-	/**
-	 * Les cases de la grille
-	 */
-	private Case[][] cases ;
-	
-	/**
-	 * On fait une première grille
-	 */
-	public Grille() {
-		this.cases = new Case[Nombre_de_lignes][Nombre_de_colonnes];
-		
-		for (int numeroDeLigne = 0; numeroDeLigne < Nombre_de_lignes; numeroDeLigne++)
-			for (int numeroDeColonne = 0; numeroDeColonne < Nombre_de_colonnes; numeroDeColonne++)
-					this.cases[numeroDeLigne][numeroDeLigne] = new Case (Case.Vide);
+        for (int numeroDeLigne = 0; numeroDeLigne < Nombre_de_lignes; numeroDeLigne++)
+            for (int numeroDeColonne = 0; numeroDeColonne < Nombre_de_colonnes; numeroDeColonne++)
+                /* XXX Erreur ! C'est numeroDeColonne ! */
+                this.cases[numeroDeLigne][numeroDeLigne] = new Case (Case.Vide);
 	}
 	
 }
