c65788acb05a86b2ade9c5d9ea3585af89b59135
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-26 21:49:39 +0100

    TODO Javadoc (Case.java)

--- src/Case.java
+++ src/Case.java
@@ -1,30 +1,15 @@
-
+/* TODO JavaDoc. */
 public class Case {
-
-	/**
-	 * La case est represente par un booleen : Vide ou touchee
-	 */
+	/** La case est represente par un booleen : vide ou touchee. */
 	public boolean EtatCase;
-	
-	/**
-	 * La case est vide
-	 */
+    /* TODO Attention, en Java, par convention, on fait "static final". */
+    /* TODO Attention, en Java, par convention, on mets les noms de constantes en majuscule. */
+	/** La case est vide. */
 	public final static boolean Vide = false;
-	
-
-	/**
-	 * La case est touchee
-	 */
+	/** La case est touchee. */
 	public final static boolean Touchee = true;
 
-	/**
-	 * @param etatCase
-	 */
 	public Case(boolean etatCase) {
-
 		EtatCase = etatCase;
-	
 	}
-	
-	
 }
