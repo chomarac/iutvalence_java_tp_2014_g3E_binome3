c3a242b6f98f05bf0a37491b7566f3cf34beef17
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-18 12:14:02 +0100

    TODO Javadoc (Grille.java)

--- src/Grille.java
+++ src/Grille.java
@@ -1,26 +1,27 @@
-/* TODO Javadoc ! Je vous mets un exemple. */
+/* TODO JavaDoc. */
+/* TODO Package. */
 
 /**
  * Grille de bataille navale.
+ *
+ * @author TODO
+ * @version TODO
  */
 public class Grille {
-	
 	/** Nombre de ligne d'une grille. */
     private static final int NOMBRE_DE_LIGNES   = 10;
     
     /** Nombre de colonne d'une grille. */
     private static final int NOMBRE_DE_COLONNES = 10;
-    
+
+    /* TODO final ? */
     /** Les cases de la grille. */
     private Case[][] cases;
 
-    /**
-     * On fait une premiere grille
-     */
     public Grille() {
-        this.cases = new Case[NOMBRE_DE_LIGNES][NOMBRE_DE_COLONNES];
-        for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++)
-            for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++)
+        this.cases = new Case[Grille.NOMBRE_DE_LIGNES][Grille.NOMBRE_DE_COLONNES];
+        for (int numeroDeLigne = 0; numeroDeLigne < Grille.NOMBRE_DE_LIGNES; numeroDeLigne++)
+            for (int numeroDeColonne = 0; numeroDeColonne < Grille.NOMBRE_DE_COLONNES; numeroDeColonne++)
                 this.cases[numeroDeLigne][numeroDeColonne] = new Case (Case.VIDE);
 	}
     
@@ -29,5 +30,4 @@ public class Grille {
     	/*Position actuelle + (taille-1) soit possible*/
     	/* Placer le bateau dans la grille. */
 	}
-	
 }
