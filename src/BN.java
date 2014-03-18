edbd4f6d921cad540e5a73af0d56bd6fe80126f8
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-18 12:13:45 +0100

    TODO Javadoc (BN.java)

--- src/BN.java
+++ src/BN.java
@@ -1,30 +1,37 @@
-/* TODO Javadoc ! */
+/* TODO JavaDoc. */
+/* TODO Package. */
+/* TODO Renommer la classe. */
+
+/**
+ * TODO
+ *
+ * @author TODO
+ * @version TODO
+ */
 public class BN {
 	private final Joueur joueur1;
 	private final Joueur joueur2;
 	
 	public BN(String nomJoueur1, String nomJoueur2) {
+        /* TODO Pourquoi ne pas utiliser vos paramétres. */
 		joueur1 = new Joueur("Papa");
 		joueur2 = new Joueur("Maman");
-		System.out.println(joueur1.getNomJoueur());
-		System.out.println(joueur2.getNomJoueur());
 	}
 
-	public void jouer() {
-		/* Demander la place des bateaux du joueur 1. */
-		try {
-			joueur1.ajouterBateau(new Croiseur(0,0,SensBateaux.EST));
-		} catch (PlacementImpossible e) {
-				
-		}
-		joueur1.afficherGrilles();
-		try {
-			joueur2.ajouterBateau(new Croiseur(2, 2, SensBateaux.SUD));
-		} catch (PlacementImpossible e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-	}
+    public void jouer() {
+        /* Demander la place des bateaux du joueur 1. */
+        try {
+            joueur1.ajouterBateau(new Croiseur(0, 0, SensBateaux.EST));
+        }
+        catch (PlacementImpossible e) {
+
+        }
+        joueur1.afficherGrilles();
+        try {
+            joueur2.ajouterBateau(new Croiseur(2, 2, SensBateaux.SUD));
+        }
+        catch (PlacementImpossible e) {
+            e.printStackTrace();
+        }
+    }
 }
